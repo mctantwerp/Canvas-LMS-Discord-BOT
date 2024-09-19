e67f60cd44bab52bbecc5b206558d0b56c189de2
dark3art <rinus.vanlinden@gmail.com>
2024-09-19 15:46:03 +0200

    exported bot init

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 
 const helperFunctions = require("./functions.js");
+const bot = require("./initBot.js");
+
+const { Client, IntentsBitField } = require("discord.js");
+
+
 
 //we use env file for secret tokens
 require("dotenv").config();
 
-//using the package discord.js
-const { Client, IntentsBitField } = require("discord.js");
 
 //Api url
 const apiUrl = "https://canvas.kdg.be/api/v1/announcements?context_codes[]=course_49719";
@@ -18,25 +21,7 @@ const requestOptions = {
     }
 };
 
-//creating a new client
-//intents are used to specify what events the bot should listen to
-//in this case, the bot will listen to guilds, guild members, guild messages, and guild message content (guild in discord is a server)
-const client = new Client({
-  intents: [
-    IntentsBitField.Flags.Guilds,
-    IntentsBitField.Flags.GuildMembers,
-    IntentsBitField.Flags.GuildMessages,
-    IntentsBitField.Flags.MessageContent,
-  ],
-});
-
-//logs the bot in using the token
-client.login(process.env.DISCORD_TOKEN);
-
-//when the bot is ready, it will log "ready" to the console
-client.on("ready", (c) => {
-  console.log(c.user.tag + " has started");
-});
+const client = bot.initBot();
 
 
 
---
+++ src/initBot.js
@@ -0,0 +1,36 @@
+
+//we use env file for secret tokens
+require("dotenv").config();
+
+//using the package discord.js
+const { Client, IntentsBitField } = require("discord.js");
+
+
+function initBot(){
+   //creating a new client
+    //intents are used to specify what events the bot should listen to
+    //in this case, the bot will listen to guilds, guild members, guild messages, and guild message content (guild in discord is a server)
+    const client = new Client({
+        intents: [
+            IntentsBitField.Flags.Guilds,
+            IntentsBitField.Flags.GuildMembers,
+            IntentsBitField.Flags.GuildMessages,
+            IntentsBitField.Flags.MessageContent,
+        ],
+    });
+  
+    //logs the bot in using the token
+    client.login(process.env.DISCORD_TOKEN);
+  
+    //when the bot is ready, it will log "ready" to the console
+    client.on("ready", (c) => {
+        console.log(c.user.tag + " has started");
+    });
+
+    //returning the client
+    return client;
+}
+
+module.exports = {
+    initBot,
+}
