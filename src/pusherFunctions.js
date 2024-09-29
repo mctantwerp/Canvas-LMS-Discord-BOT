87b218447270f9098e0588bd147b3062ab874429
Tobias Oostvogels <tobias.oostvogels@student.kdg.be>
2024-09-29 12:05:39 +0200

    working with env file

--- src/pusherFunctions.js
+++ src/pusherFunctions.js
@@ -3,11 +3,11 @@ const Pusher = require('pusher');
 
 function sendDataToPi(stringMessage){
     const pusher = new Pusher({
-        appId: "1867626",
-        key: "e91b5acb99e7070669a0",
-        secret: "eaafc85da66e7f9b8839",
-        cluster: "eu",
-        useTLS: true
+        appId: process.env.PUSHER_APP_ID,
+        key: process.env.PUSHER_KEY,
+        secret: process.env.PUSHER_SECRET,
+        cluster: process.env.PUSHER_CLUSTER,
+        useTLS: process.env.PUSHER_USE_TLS
       });
       
       pusher.trigger("raspberry", "message", {
